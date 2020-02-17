352d0c92a69fb5c7e977dea20813351bdc8a3216
tensquare2443 <owner@Kevins-MacBook-Pro.local>
2020-02-16 23:54:43 -0500

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# Home Screen  
+
+Uses: Vue, Vue.Draggable (https://www.npmjs.com/package/vuedraggable)
---
+++ src/components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <img
+      :src="require(`../assets/icons/${file}`)"
+      alt=""
+      class="app__icon"
+      :class="{
+        selecting: selectedIcon,
+        selected: selectedIcon.name === name
+      }"
+    />
+    <div class="app__label">
+      <p class="app__label-txt">{{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// "-webkit-user-select: none" on ".app" is messing up firefox
+export default {
+  props: ["name", "file", "selectedIcon"]
+};
+</script>
